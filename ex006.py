312d6f270d2507a790843e3570af72118feb8441
LuisEduardo-oss <luizaopokas@gmail.com>
2021-06-05 23:27:43 -0300

    Create ex006.py

---
+++ ex006.py
@@ -0,0 +1,6 @@
+from math import sqrt
+num = int(input('digite um numero: '))
+dobro = num * 2
+triplo = num * 3
+raiz = sqrt(num)
+print('dobro: {}\ntriplo: {}\nraiz quadrada: {}'.format(dobro, triplo, raiz))
