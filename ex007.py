700df7759b9a9e3a7fe68b3994f5ce742b35f8a1
LuisEduardo-oss <luizaopokas@gmail.com>
2021-06-05 23:28:09 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,4 @@
+n1 = float(input('qual foi a primeira nota? '))
+n2 = float(input('qual foi a segunda nota? '))
+media = (n1 + n2) / 2
+print('a media entre {} e {} é igual a {:.1f}'.format(n1, n2, media))
