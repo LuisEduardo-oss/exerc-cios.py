5022fc71d71e884901898cb1624e4b496266ea0d
LuisEduardo-oss <luizaopokas@gmail.com>
2021-06-07 00:06:45 -0300

    Create ex027.py
    Primeiro e último nome de uma pessoa

---
+++ ex027.py
@@ -0,0 +1,5 @@
+n = str(input('qual o seu nome completo? ')).strip()
+nome = n.split()
+print('muito prazer em te comhecer!')
+print('seu primeiro nome é: {}'.format(nome[0]))
+print('seu ultimo nome é: {}'.format(nome[len(nome)- 1]))
