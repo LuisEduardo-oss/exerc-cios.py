f3149d46fb1e862d2c25a4e5102348c81ac6d84e
LuisEduardo-oss <luizaopokas@gmail.com>
2021-06-24 23:46:06 -0300

    Create ex038.py

---
+++ ex038.py
@@ -0,0 +1,8 @@
+n1 = int(input('digite um numero inteiro: '))
+n2 = int(input('digite outro numero inteiro: '))
+if n1 > n2:
+    print('o PRIMEIRO valor é maior')
+elif n1 < n2:
+    print('o SEGUNDO valor é maior')
+else:
+    print('os DOIS valores são iguais')
