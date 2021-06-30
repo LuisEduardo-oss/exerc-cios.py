4418acea195991e986693bd93c16b2680dce31f3
LuisEduardo-oss <luizaopokas@gmail.com>
2021-06-29 21:24:56 -0300

    Revert "Create ex028.py"
    This reverts commit b1b8072201239acbf11121a5dd01a6a2f15b56b2.

--- ex028.py
+++
@@ -1,8 +0,0 @@
-import random
-n = int(input('digite um numero:'))
-n1 = random.choice([0,5])
-if n == n1:
-    print('voce acertou, PARABENS')
-else:
-    print('voce errou:/')
-print('o numero era {}'.format(n1))
