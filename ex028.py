1ad6bc010cda4d27265764f8186cfa6940c4d9b8
LuisEduardo-oss <luizaopokas@gmail.com>
2021-06-29 21:26:33 -0300

    Create ex028.py

---
+++ ex028.py
@@ -0,0 +1,8 @@
+import random
+n = int(input('digite um numero:'))
+n1 = random.uniform(0,5)
+if n == n1:
+    print('voce acertou, PARABENS')
+else:
+    print('voce errou:/')
+print('o numero era {}'.format(n1))
